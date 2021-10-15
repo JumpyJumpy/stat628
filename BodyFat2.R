8793c615e017731338e5a46cb6be2220aa82e863
Shubo Lin <slin268@wisc.edu>
2021-10-14 23:37:14 -0500

    Add files via upload

--- BodyFat2.R
+++ BodyFat2.R
@@ -8,16 +8,13 @@ summary(BodyFat)
 BodyFat$HEIGHT <- BodyFat$HEIGHT / 0.3937
 
 ## delete bad data points, drop density, BMI
-BodyFat_cleaned <- subset(BodyFat[-c(42,172,182),], select = -c(1,3,7))
-#write_csv(BodyFat_cleaned, "./data/BodyFat_cleaned.csv")
-corr <- cor(BodyFat_cleaned)
+BodyFat_cleaned <- subset(BodyFat[-c(42, 172, 182),], select = -c(1, 3, 7))
+corr <- cor(BodyFat_cleaned[,-1])
 
-
-
-## full model
+## initial model
 lm_model1 <- lm(BODYFAT~., data = BodyFat_cleaned)
 summary(lm_model1)
-vif(lm_model1)
+vif(lm_model1) ## weight is dropped due to high vif
 head(BodyFat_cleaned)
 ## NO.39 is not an outlier
 par(mfrow = c(2,2))
@@ -27,20 +24,31 @@ plot(lm_model1)
 lm_model2 <- lm(BODYFAT ~ . - WEIGHT, data = BodyFat_cleaned)
 lm_model2 <- stepAIC(lm_model2, direction = "both", trace = FALSE)
 summary(lm_model2)
-vif(lm_model2)
+vif(lm_model2) ## HIP is dropped due to high vif
 
-lm_model3 <- lm(BODYFAT ~ . - WEIGHT - HIP , data = BodyFat_cleaned)
-lm_model3 <- stepAIC(lm_model3, direction = "both", trace = FALSE)
-summary(lm_model3)
-vif(lm_model3)
 
-lm_model4 <- lm(BODYFAT ~ AGE + HEIGHT + ABDOMEN + FOREARM + WRIST, data = BodyFat_cleaned)
-#lm_model4 <- stepAIC(lm_model4, direction = "both", trace = FALSE)
-summary(lm_model4)
-vif(lm_model4)
+## best subset selection
+library("leaps")
+best_subset <- regsubsets(BODYFAT ~. - WEIGHT - HIP, data = BodyFat_cleaned)
+summary(best_subset)
+summary(best_subset)$bic
+summary(best_subset)$adjr2 # (3) & (4) are candidates of optimal model since it has lowest BIC and a relatively high R^2
 
-lm_model5 <- lm(BODYFAT ~ AGE + ABDOMEN + WRIST, data = BodyFat_cleaned)
-#lm_model5 <- stepAIC(lm_model5, direction = "both", trace = FALSE)
-summary(lm_model5)
-vif(lm_model5)
-final <- lm_model5
+
+final <- lm(BODYFAT ~ ABDOMEN + WRIST + HEIGHT, data = BodyFat_cleaned)
+summary(final)
+vif(final)
+plot(final)
+
+
+
+#BodyFat_cleaned <- BodyFat_cleaned[-39,]
+final <- lm(BODYFAT ~ ABDOMEN + HEIGHT + WRIST, data = BodyFat_cleaned)
+summary(final)
+vif(final)
+plot(final)
+
+
+
+## AGE: ON    R^2 = 
+## AGE: OFF   R^2 = 0.7094
